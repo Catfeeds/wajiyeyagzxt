d9bf9fa41d3dec39261e333471b46a072a0408b9
leefon <lfphper@126.com>
2018-01-04 18:12:58 +0800

    2018-01-04

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,6 +32,182 @@ Page({
     cname1: '',
     cname2: '',
     cname3:'',
+    gongpage: 1,
+    qiupage: 1,
+    showModalStatus:false,
+    gong:[],
+    qiu:[]
+  },
+  // 上传图片
+  chooseImage: function () {
+    var that = this
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var imageSrc = res.tempFilePaths;
+
+        that.setData({
+          imgs: imageSrc
+        })
+      }
+    })
+
+  },
+  //我要发布
+  bindFormSubmit: function (e) {
+    var that = this;
+    var content = e.detail.value.content;
+    that.setData({
+      content: content
+    });
+    wx.showLoading({
+      title: '发布中...',
+    })
+    that.upload();
+  },
+  upload: function () {
+    var that = this;
+    var imageSrc = that.data.imgs;
+    var i = that.data.i;
+    var length = imageSrc.length;
+    wx.uploadFile({
+      url: app.pubData.hostUrl + '/Api/User/uploadimg',
+      filePath: imageSrc[i],
+      name: 'data',
+      success: function (res) {
+        console.log('uploadImage success, res is:', res);
+        that.setData({
+          photo: that.data.photo.concat(res.data) + ',',
+          i: that.data.i + 1,
+        })
+        if (that.data.i < length) {
+          that.upload();
+        } else {
+          that.save();
+        }
+      },
+
+      fail: function ({ errMsg }) {
+        console.log('uploadImage fail, errMsg is', errMsg)
+        return false;
+      }
+    })
+  },
+  save: function () {
+    var that = this;
+    var content = that.data.content;
+    var dtype = that.data.dtype;
+    if (!content) {
+      wx.showToast({
+        title: '请输入供求内容！',
+        duration: 2000
+      });
+      return false;
+    }
+    console.log(dtype)
+    if (dtype < 1 || dtype > 2) {
+      wx.showToast({
+        title: '网络异常，请稍后再试！',
+        duration: 2000
+      });
+      return false;
+    }
+    wx.request({
+      url: app.pubData.hostUrl + '/Api/User/supply',
+      method: 'post',
+      data: {
+        content: content,
+        dtype: dtype,
+        uid: app.pubData.userId,
+        photo_x: that.data.photo
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var status = res.data.status;
+        if (status == 1) {
+          wx.hideLoading();
+          wx.showToast({
+            title: '发布成功！',
+            duration: 2000
+          });
+          that.setData({
+            imgs: '',
+            img_save: '',
+            i: 0,
+            photo: '',
+            content: '',
+            showModalStatus: false
+          });
+          that.initIndexData();
+        } else {
+          wx.showToast({
+            title: res.data.err,
+            duration: 2000
+          });
+        }
+      },
+      fail: function (e) {
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      },
+    })
+
+
+
+  },
+  // 弹窗
+  setModalStatus: function (e) {
+    var dtype = e.currentTarget.dataset.dtype;
+    this.setData({
+      dtype: dtype
+    });
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export()
+    })
+    if (e.currentTarget.dataset.status == 1) {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation
+      })
+      if (e.currentTarget.dataset.status == 0) {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+  },
+
+  buttonClick (e) {
+    this.setData({
+      showModalStatus: true
+    })
+  },
+  setModalStatus (e) {
+    this.setData({
+      showModalStatus:false
+    })
   },
   tabClick (e) {
     this.setData({
@@ -39,11 +215,11 @@ Page({
     });
     if (e.target.dataset.id == 0){
       this.setData({
-        supply: this.data.sale
+        supply: this.data.gong
       });
     }else{
       this.setData({
-        supply: this.data.buy
+        supply: this.data.qiu
       });
     }
   },
@@ -97,11 +273,13 @@ Page({
           list4:res.data.list4,
           buy:res.data.buy,
           sale:res.data.sale,
-          supply:res.data.sale,
           news:res.data.news,
           cname1: res.data.cname1,
           cname2: res.data.cname2,
           cname3:res.data.cname3,
+          supply: res.data.gong,
+          gong:res.data.gong,
+          qiu:res.data.qiu
         })
       }
     })
@@ -148,6 +326,111 @@ Page({
    */
   onReachBottom: function () {
     
+  },
+
+  //上一页
+  lastpage: function (e) {
+    var that = this;
+    var ptype = e.currentTarget.dataset.ptype;
+    if (ptype == 1) {
+      var page = that.data.gongpage;
+    } else {
+      var page = that.data.qiupage;
+    }
+    if (page <= 1) {
+      wx.showToast({
+        title: '已经是第一页了！',
+        duration: 2000
+      });
+      return false;
+    }
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Index/getpage',
+      method: 'post',
+      data: { page: page - 1, ptype: ptype },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var list = res.data.list;
+        if (list == '') {
+          wx.showToast({
+            title: '没有找到更多数据',
+            duration: 2000
+          });
+          return false;
+        }
+        if (ptype == 1) {
+          that.setData({
+            gong: list,
+            supply: list,
+            gongpage: page - 1
+          });
+        } else {
+          that.setData({
+            qiu: list,
+            supply: list,
+            qiupage: page - 1
+          });
+        }
+        //endInitData
+      },
+      fail: function (e) {
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      },
+    })
+  },
+
+  //下一页
+  nextpage: function (e) {
+    var that = this;
+    var ptype = e.currentTarget.dataset.ptype;
+    if (ptype == 1) {
+      var page = that.data.gongpage;
+    } else {
+      var page = that.data.qiupage;
+    }
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Index/getpage',
+      method: 'post',
+      data: { page: page + 1, ptype: ptype },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var list = res.data.list;
+        if (list == '') {
+          wx.showToast({
+            title: '已经是最后一页了！',
+            duration: 2000
+          });
+          return false;
+        }
+        if (ptype == 1) {
+          that.setData({
+            gong: list,
+            supply: list,
+            gongpage: page + 1
+          });
+        } else {
+          that.setData({
+            qiu: list,
+            supply: list,
+            qiupage: page + 1
+          });
+        }
+        //endInitData
+      },
+      fail: function (e) {
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      },
+    })
   },
 
   /**
